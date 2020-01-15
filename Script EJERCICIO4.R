6003fea918de5fe9ba0571bf2cb4fe998571abc4
andrealepera <59932534+andrealepera@users.noreply.github.com>
2020-01-15 15:27:58 -0300

    Create Script EJERCICIO4.R

---
+++ Script EJERCICIO4.R
@@ -0,0 +1,14 @@
+#EJERCICIO4
+x=c(rep(1,10),rep(1.5,15),rep(2,20),rep(3,25),rep(3.5,25),4)
+boxplot(x,horizontal = T)
+
+y=0.252*x-10
+round(summary(y),2)
+paste("sd(y)=",round(sd(y),2))
+round(quantile(y,c(0.01,0.10,0.2,0.35,0.45,0.80,0.90)),2)
+
+
+
+
+
+
